62f09e23fd99a67b11854729fa7f637a19b956c4
Amirul Muminin <almin.cesc@gmail.com>
2025-12-30 17:34:17 +0700

    feat: refactor movement between buffers with leader instead of ctrl

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,61 +1,61 @@
 require "nvchad.mappings"
--- add yours here
-local map = vim.keymap.set
 
+local map = vim.keymap.set
+local del = vim.keymap.del
+
+-- 1. UNBIND/DELETE DEFAULT NVCHAD MAPPINGS
+-- Kita hapus mapping default Ctrl + h/j/k/l agar tidak konflik atau membingungkan
+del("n", "<C-h>")
+del("n", "<C-j>")
+del("n", "<C-k>")
+del("n", "<C-l>")
+
+-- 2. CUSTOM MAPPINGS
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("t", "jk", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- Dari Normal Mode: Simpan (tetap di Normal Mode)
+-- Simpan File
 map("n", "<C-s>", "<cmd> w <CR>", { desc = "Save file" })
-
--- Dari Insert Mode: Simpan dan pindah ke Normal Mode
--- Kita gunakan <Esc> setelah simpan untuk menjamin keluar dari Insert Mode
 map("i", "<C-s>", "<Esc><cmd> w <CR>", { desc = "Save file and go to normal mode" })
-
-map("n", "<leader>x", function()
-  -- Menghitung jumlah buffer yang valid/terbuka
-  local bufs = vim.fn.getbufinfo({ buflisted = 1 })
-
-  if #bufs <= 1 then
-    -- Jika hanya sisa 1 buffer, tutup Neovim sepenuhnya
-    vim.cmd "quit"
-  else
-    -- Jika masih ada banyak buffer, gunakan fungsi bawaan NvChad untuk tutup buffer saja
-    require("nvchad.tabufline").close_buffer()
-  end
-end, { desc = "Close buffer or quit nvim" })
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
--- Mode Normal: Pindahkan baris aktif ke atas/bawah
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
-
--- Mode Visual: Pindahkan blok yang diseleksi ke atas/bawah
-map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-
--- Memaksa kursor selalu berada di posisi yang sama di layar setelah bergerak
-map("n", "j", "jzz", { noremap = true })
-map("n", "k", "kzz", { noremap = true })
-
--- Mengubah leader e menjadi toggle file tree (sama seperti Ctrl + n)
-map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle nvimtree" })
 map("n", "<leader>s", "<cmd> w <CR>", { desc = "Save file" })
 
--- Leader + h untuk toggle terminal horizontal (sama seperti Alt + h)
-map("n", "<leader>h", function()
+-- Navigasi Window (Ganti Ctrl ke Leader)
+map("n", "<leader>h", "<C-w>h", { desc = "Window left" })
+map("n", "<leader>j", "<C-w>j", { desc = "Window down" })
+map("n", "<leader>k", "<C-w>k", { desc = "Window up" })
+map("n", "<leader>l", "<C-w>l", { desc = "Window right" })
+
+-- Toggle Terminal (Sekarang menggunakan <leader>tt)
+map({ "n", "t" }, "<leader>tt", function()
   require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
 end, { desc = "Terminal Toggle Horizontal" })
 
--- Membuka LazyGit
+-- Close Buffer atau Quit
+map("n", "<leader>x", function()
+  local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+  if #bufs <= 1 then
+    vim.cmd "quit"
+  else
+    require("nvchad.tabufline").close_buffer()
+  end
+end, { desc = "Close buffer or quit nvim" })
+
+-- Move Lines (Alt + j/k)
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Cursor Centering
+map("n", "j", "jzz", { noremap = true })
+map("n", "k", "kzz", { noremap = true })
+
+-- File Tree & Git
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle nvimtree" })
 map("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Toggle LazyGit" })
 
--- Select All di Normal Mode
+-- Select All
 map("n", "<C-a>", "ggVG", { desc = "Select all" })
-
--- Select All di Insert Mode (otomatis pindah ke Visual Mode)
 map("i", "<C-a>", "<Esc>ggVG", { desc = "Select all" })
-
--- Select All di Visual Mode (untuk memastikan tetap ter-select)
 map("v", "<C-a>", "<Esc>ggVG", { desc = "Select all" })
